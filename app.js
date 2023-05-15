46ea718b6642429b9717d55e256cde3fbd29aff0
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-15 14:37:03 -0300

    se quita la lógica de multiplicar

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
-const fs = require('node:fs');
+const { crearArchivo } = require('./helpers/multiplicar')
+
 console.clear()
 
-let salida = '';
 base = 3;
 
-console.log('====================================');
-console.log(`tabla del:${base}`);
-console.log('====================================');
-
-for (let i = 1; i <= 10; i++) {
-    salida += `${base} x ${i} = ${base * i}\n`;
-};
-
-fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
-    if (err) throw err;
-    console.log(`tabla-${base}.txt creado`);
-})
+crearArchivo(base)
+    .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
+    .catch(err => console.log(err));
