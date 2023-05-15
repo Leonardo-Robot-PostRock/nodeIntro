9849b2af79d8a177065f7768edd80e9ca1ee8631
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-15 14:23:24 -0300

    se crea una tabla txt de multiplicar

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+const fs = require('node:fs');
+console.clear()
 
-function tabla(n) {
-    for (let i = 1; i < 11; i++) {
-        console.log(n, ' x ', i, ' = ', n * i,);
-    };
-}
+let salida = '';
+base = 3;
 
-tabla(5);
+console.log('====================================');
+console.log(`tabla del:${base}`);
+console.log('====================================');
+
+for (let i = 1; i <= 10; i++) {
+    salida += `${base} x ${i} = ${base * i}\n`;
+};
+
+fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+    if (err) throw err;
+    console.log(`tabla-${base}.txt creado`);
+})
