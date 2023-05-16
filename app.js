fa61547a0de91962514be4a3534cb131f331a542
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-15 23:06:44 -0300

    recibir información desde línea de comando

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const { crearArchivo } = require('./helpers/multiplicar')
 
 console.clear()
 
-base = 3;
+// base = 3;
+
+console.log(process.argv);
+const [, , arg3 = 'base=5'] = process.argv;
+const [, base = 5] = arg3.split('=');
 
 crearArchivo(base)
     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
