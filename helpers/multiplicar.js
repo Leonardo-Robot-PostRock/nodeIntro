ac29ac627d5a28be36745a4d16456e67709550e7
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-15 23:05:54 -0300

    se agrega console.log()

--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,28 +1,5 @@
 const fs = require('node:fs');
 
-// const crearArchivo = (base = 5) => {
-//     return new Promise((resolve, reject) => {
-//         console.log('====================================');
-//         console.log('tabla del: ', base);
-//         console.log('====================================');
-
-//         let salida = '';
-//         for (let i = 1; i <= 10; i++) {
-//             salida += `${base} x ${i} = ${base * i}\n`;
-//         };
-
-//         // fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
-//         //     if (err) throw err;
-//         //     console.log(`tabla-${base}.txt creado`);
-//         // })
-//         fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
-//             (err)
-//                 ? reject(err)
-//                 : resolve(`tabla-${base}.txt`)
-//         });
-//     })
-// }
-
 const crearArchivo = async (base = 5) => {
     try {
         console.log('====================================');
@@ -34,10 +11,8 @@ const crearArchivo = async (base = 5) => {
             salida += `${base} x ${i} = ${base * i}\n`;
         };
 
-        // fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
-        //     if (err) throw err;
-        //     console.log(`tabla-${base}.txt creado`);
-        // })
+        console.log(salida);
+
         fs.writeFileSync(`tabla-${base}.txt`, salida)
         return `tabla-${base}.txt`
     } catch (error) {
