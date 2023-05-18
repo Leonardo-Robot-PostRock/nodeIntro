390ee1ac230515ab11ba2070ae33aef0a5399634
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-18 14:50:56 -0300

    separación de la lógica

--- app.js
+++ app.js
@@ -1,28 +1,7 @@
 const { crearArchivo } = require('./helpers/multiplicar')
-const argv = require('yargs')
-    .option('b', {
-        alias: 'base',
-        type: 'number',
-        demandOption: true
-    })
-    .option('l', {
-        alias: 'listar',
-        type: 'boolean',
-        demandOption: true,
-        default: false
-    })
-    .check((argv, options) => {
-        if (isNaN(argv.b)) {
-            throw 'La base tiene que ser un número'
-        }
-        return true;
-    })
-    .argv;
+const argv = require('./config/yargs');
+
 console.clear()
-
-// base = 3;
-
-console.log(argv);
 
 // console.log(process.argv);
 // const [, , arg3 = 'base=5'] = process.argv;
