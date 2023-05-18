e204c870bb64c4d0eddf9417721c5d67a893febc
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-18 16:57:51 -0300

    se agregan colores

--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -11,27 +11,33 @@ colors.setTheme({
     help: 'cyan',
     warn: 'yellow',
     debug: 'blue',
-    error: 'red'
+    error: 'red',
 })
 
 const crearArchivo = async (base = 5, listar = false) => {
+    x = 'x'.info;
+    const header = `TABLA DEL: ${base}`.underline.gray.bgYellow;
     try {
         let salida = '';
+
         for (let i = 1; i <= 10; i++) {
-            salida += `${base} x ${i} = ${base * i}\n`;
+            total = `${base * i}`.random
+            multiplicacion = `$${base} ${x} ${i} = ${total}`
+            margen = ' '.repeat(12.5)
+            salida += `${margen}${base} ${x} ${i} = ${total}${margen}\n`.bold;
         };
 
         if (listar) {
-            console.log('====================================');
-            console.log('TABLA DEL: '.gray, base);
-            console.log('====================================');
+            console.log('='.repeat(36).rainbow);
+            console.log(`${margen}${header}${margen}`);
+            console.log('='.repeat(36).rainbow);
             console.log(salida.debug);
         }
 
         fs.writeFileSync(`tabla-${base}.txt`, salida)
-        return `tabla-${base}.txt`.rainbow
+        return `tabla-${base}.txt`.bgWhite.debug
     } catch (error) {
-        throw error;
+        throw error.error;
     }
 }
 
