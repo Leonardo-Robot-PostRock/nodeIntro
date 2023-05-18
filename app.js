38d051e18f82431bd06864c638e85bb58dfdceae
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-18 14:39:57 -0300

    uso del option y check de la api de yargs

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const { crearArchivo } = require('./helpers/multiplicar')
-const argv = require('yargs').argv;
+const argv = require('yargs')
+    .option('b', {
+        alias: 'base',
+        type: 'number',
+        demandOption: true
+    })
+    .option('l', {
+        alias: 'listar',
+        type: 'boolean',
+        demandOption: true,
+        default: false
+    })
+    .check((argv, options) => {
+        if (isNaN(argv.b)) {
+            throw 'La base tiene que ser un número'
+        }
+        return true;
+    })
+    .argv;
 console.clear()
 
 // base = 3;
 
-console.log(process.argv);
 console.log(argv);
-console.log('base: yargs', argv.base);
 
 // console.log(process.argv);
 // const [, , arg3 = 'base=5'] = process.argv;
 // const [, base = 5] = arg3.split('=');
 
-// crearArchivo(base)
-//     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
-//     .catch(err => console.log(err));
+crearArchivo(argv.b, argv.l)
+    .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
+    .catch(err => console.log(err));
