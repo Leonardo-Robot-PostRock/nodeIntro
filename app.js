230c6dfd0b4729e5444cf157dbeca0d1380ae1b5
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-18 13:02:01 -0300

    se prueban los argumentos de process y yargs. argumentos de lineas de comando

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const { crearArchivo } = require('./helpers/multiplicar')
-
+const argv = require('yargs').argv;
 console.clear()
 
 // base = 3;
 
 console.log(process.argv);
-const [, , arg3 = 'base=5'] = process.argv;
-const [, base = 5] = arg3.split('=');
+console.log(argv);
+console.log('base: yargs', argv.base);
 
-crearArchivo(base)
-    .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
-    .catch(err => console.log(err));
+// console.log(process.argv);
+// const [, , arg3 = 'base=5'] = process.argv;
+// const [, base = 5] = arg3.split('=');
+
+// crearArchivo(base)
+//     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
+//     .catch(err => console.log(err));
