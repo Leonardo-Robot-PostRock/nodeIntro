aeb7384481d8bd6dec35093523817ac552a0c984
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-18 17:11:52 -0300

    correccion de los colores

--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -15,16 +15,14 @@ colors.setTheme({
 })
 
 const crearArchivo = async (base = 5, listar = false) => {
-    x = 'x'.info;
     const header = `TABLA DEL: ${base}`.underline.gray.bgYellow;
     try {
         let salida = '';
 
         for (let i = 1; i <= 10; i++) {
-            total = `${base * i}`.random
-            multiplicacion = `$${base} ${x} ${i} = ${total}`
+            total = `${base * i}`
             margen = ' '.repeat(12.5)
-            salida += `${margen}${base} ${x} ${i} = ${total}${margen}\n`.bold;
+            salida += `${margen}${base} x ${i} = ${total}${margen}\n`;
         };
 
         if (listar) {
@@ -35,7 +33,7 @@ const crearArchivo = async (base = 5, listar = false) => {
         }
 
         fs.writeFileSync(`tabla-${base}.txt`, salida)
-        return `tabla-${base}.txt`.bgWhite.debug
+        return `tabla-${base}.txt`.bgWhite.gray
     } catch (error) {
         throw error.error;
     }
